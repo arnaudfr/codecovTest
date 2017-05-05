d7196e9e9ca32076aa20b29cbc8e3962058f32c3
arnaud <arnaud.frossard@gmail.com>
2017-05-05 14:39:20 +0200

    Initial commit

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from src.main import Main
+
+class TestMain(unittest.TestCase):
+    def test_sayHello(self):
+        main = Main()
+        self.assertEqual(main.sayHello(), "hello world")
